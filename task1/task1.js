8dd806cfa0c3369225a4c46572677ae32a04e6d8
heatclift77 <adityapratamaputra74@gmail.com>
2021-02-15 21:50:51 +0700

    second commit

---
+++ task1/task1.js
@@ -0,0 +1,39 @@
+// task 1
+let biodata = {
+    name : "aditya pratama",
+    age : 23,
+    hobbies :["main gitar, musik, game, technology"],
+    IsMaried : false,
+    schoolList : [
+        {
+            name : "MADRASA 09",
+            yearIn : 2004,
+            yearOut : 2010
+            },
+        {
+            name : "SMPN 03",
+            yearIn : 2010,
+            yearOut : 2013
+            },
+        {
+            name : "SMK PGRI 01",
+            yearIn : 2013,
+            yearOut : 2016
+            }
+        ],
+    skills :  [
+        {
+            skillName : "javascript",
+            level : "intermediet"
+        },
+        {
+            skillName : "css",
+            level : "intermediet"
+        },
+        {
+            skillName : "HTML",
+            level : "intermediet"
+        }
+        ],
+        interestInCoding : true
+    }
