bc2908fca5a875ebf3f5d8523ebe668dc85fcead
heatclift77 <adityapratamaputra74@gmail.com>
2021-02-15 21:54:25 +0700

    second commit

---
+++ task4/task4.js
@@ -0,0 +1,25 @@
+// task 4
+let data = {
+    id : 1,
+    name : "leanne graham",
+    username : "bret",
+    email : "sincere@april.biz",
+    adress : {
+        street : "kulas light",
+        suite : "apt. 556",
+        city : "gweenborough",
+        zipcode : "92998-3874"
+    },
+    phone : "1-770-736-8031 x56442",
+    website : "hildegard.org"
+}
+
+  // a
+let dataku = {...data, 
+    name : "aditya",
+    email : "aditya@gmail.com"
+}
+  // b
+let adrees = dataku.adress
+let {street, city} = adrees
+console.log(biodata.schoolList[2])
